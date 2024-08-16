069423d1d5b6ac9b598d24d0b49952e1a6a6e724
sajid islam <sajidisalm941@gmail.com>
2024-08-16 11:18:57 +0600

    created basic express server

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
+//Z78iYttG4O69yVoO
+//product_catalog_user
+
+// Middleware
+app.use(cors())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xweyicz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req,res)=>{
+    res.send("PRODUCT CATALOG SERVER IN RUNNING")
+})
+
+app.listen(port,()=>{
+    console.log("PRODUCT CATALOG SERVER RUNNING ON",port);
+})
