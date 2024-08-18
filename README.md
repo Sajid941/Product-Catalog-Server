e6dbc4530435b9de06543fd881f787bdcf24ffd8
sajid islam <sajidisalm941@gmail.com>
2024-08-18 16:44:31 +0600

    update README.md

---
+++ README.md
@@ -0,0 +1,112 @@
+# Gadget Galaxy Server
+
+This project is a fully responsive e-commerce web application designed to provide a seamless shopping experience for users. It allows users to search, filter, and sort products across various categories, including brand, price range, and product type. The application features a dynamic product listing with advanced filtering options, enabling users to quickly find products that match their preferences.
+
+## Website live link
+### [https://product-catalog-9b837.web.app](https://product-catalog-9b837.web.app/)
+
+## Server link
+### [https://product-catalog-server.vercel.app](https://product-catalog-server.vercel.app)
+
+## Client Side Repo
+### [https://github.com/Sajid941/Product-Catalog-Client](https://github.com/Sajid941/Product-Catalog-Client)
+
+## Featured
+
+- **Dynamic Filtering**: Supports filtering by brand name, category, and price range.
+- **Sorting Functionality**: Allows sorting products by price (low-high, high-low) and by date added (newest).
+- **Text Search**: Implements case-insensitive search across product names.
+- **Pagination Support**: Efficiently handles large datasets with pagination.
+- **Product Count Retrieval**: Provides an endpoint to dynamically fetch the total number of products.
+- **MongoDB Integration**: Utilizes MongoDB for data storage with efficient querying.
+
+## Technologies
+
+### Server Side :
+- Node JS
+- Express JS
+- MongoDB
+
+### Client Side : 
+- React
+- Tailwind css
+- Firebase
+
+## Installation
+
+### Step 1:
+```bash
+# Clone the repo
+git clone https://github.com/Sajid941/Product-Catalog-Server.git # Or download from github
+```
+
+### Step 2:
+```bash
+# Install dependencies
+npm install
+```
+### Step 3: Setup environment keys
+```bash
+# .env.local Example
+# MongoDB Database username & password:
+DB_USER = MongoDB database username
+DB_PASS = MongoDb database password
+```
+
+### Step 4:
+```bash
+#  Run the server
+node index.js
+```
+
+## Contributing to the Backend
+
+### Step 1:
+```bash
+# Fork the repository
+# Go to the original repository on GitHub and click the "Fork" button in the upper right-hand corner.
+
+```
+
+### Step 2:
+```bash
+# Clone your fork
+git clone https://github.com/your-username/Product-Catalog-Server.git
+cd Product-Catalog-Server
+
+```
+
+### Step 3:
+```bash
+# Add the original repository as an upstream remote
+git remote add upstream https://github.com/Sajid941/Product-Catalog-Server.git
+
+```
+
+
+### Step 4:
+```bash
+# Create a new branch for your feature or bug fix
+git checkout -b feature-branch-name
+
+
+```
+
+### Step 5:
+```bash
+# Make your changes
+```
+
+### Step 5:
+```bash
+# Commit your changes
+git commit -m "Description of changes"
+
+```
+### Step 5:
+```bash
+# Push to your branch
+git push origin feature-branch-name
+
+
+```
--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 3000
-
-//Z78iYttG4O69yVoO
-//product_catalog_user
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
+app.use(express.json()); // Enable parsing of JSON data in request bodies
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xweyicz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with options for the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,70 +22,73 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the MongoDB server (optional starting in v4.7)
     // await client.connect();
 
+    // Reference to the database and collection
+    const productCatalogDB = client.db('ProductCatalogDB');
+    const productsCollection = productCatalogDB.collection('products');
 
-    const productCatalogDB = client.db('ProductCatalogDB')
-    const productsCollection = productCatalogDB.collection('products')
-
+    // API route to get the total count of products
     app.get('/productsCount', async (req, res) => {
-      const count = await productsCollection.estimatedDocumentCount()
-      res.send({ count })
-    })
+      const count = await productsCollection.estimatedDocumentCount(); // Get an estimate of the number of documents in the collection
+      res.send({ count }); // Send the count as a JSON response
+    });
 
+    // API route to get a paginated list of products with filters and sorting
     app.get('/products', async (req, res) => {
-      const page = parseInt(req.query.page)
-      const size = parseInt(req.query.size)
-      const sortOption = req.query.sort;
-      const searchQuery = req.query.query;
-      const brandName = req.query.brandName;
-      const category = req.query.category;
-      const minPrice = parseInt(req.query.minimumPrice) || 0 ;
-      const maxPrice = parseInt(req.query.maximumPrice) || Number.MAX_SAFE_INTEGER
-      const skip = (page - 1) * size
-      console.log(minPrice,maxPrice);
+      const page = parseInt(req.query.page); // Get the page number from query params
+      const size = parseInt(req.query.size); // Get the number of items per page from query params
+      const sortOption = req.query.sort; // Get the sorting option from query params
+      const searchQuery = req.query.query; // Get the search query from query params
+      const brandName = req.query.brandName; // Get the brand name filter from query params
+      const category = req.query.category; // Get the category filter from query params
+      const minPrice = parseInt(req.query.minimumPrice) || 0; // Get the minimum price filter from query params, default to 0
+      const maxPrice = parseInt(req.query.maximumPrice) || Number.MAX_SAFE_INTEGER; // Get the maximum price filter from query params, default to max safe integer
+      const skip = (page - 1) * size; // Calculate the number of documents to skip based on the current page
 
-      let sort = {}
+      console.log(minPrice, maxPrice);
+
+      // Define sorting logic based on the selected option
+      let sort = {};
       switch (sortOption) {
         case 'low-high':
-          sort = { price: 1 }
+          sort = { price: 1 }; // Sort by price in ascending order
           break;
         case 'high-low':
-          sort = { price: -1 }
+          sort = { price: -1 }; // Sort by price in descending order
           break;
         case 'newest':
-          sort = { date_added: -1 }
+          sort = { date_added: -1 }; // Sort by the date added in descending order
           break;
         case 'auto':
-          sort = {}
+          sort = {}; // No sorting
           break;
         default:
-          sort = {}
+          sort = {}; // Default to no sorting
       }
-      let query = {}
 
-      query = {
+      // Define the query object for filtering products
+      let query = {
         $and: [
-          { name: { $regex: searchQuery ? searchQuery : "", $options: 'i' } },
-          brandName ? { company_name: { $regex: brandName, $options: 'i' } } : {},
-          category ? { category: { $regex: category, $options: 'i' } } : {},
-          {price:{$gte:minPrice, $lte:maxPrice}}
+          { name: { $regex: searchQuery ? searchQuery : "", $options: 'i' } }, // Search by product name using a case-insensitive regex
+          brandName ? { company_name: { $regex: brandName, $options: 'i' } } : {}, // Filter by brand name if provided
+          category ? { category: { $regex: category, $options: 'i' } } : {}, // Filter by category if provided
+          { price: { $gte: minPrice, $lte: maxPrice } } // Filter by price range
         ]
+      };
 
-      }
-
-
+      // Fetch the filtered and sorted products from the database with pagination
       const result = await productsCollection.find(query)
-        .skip(skip)
-        .limit(size)
-        .sort(sort)
-        .toArray()
+        .skip(skip) // Skip documents for pagination
+        .limit(size) // Limit the number of documents to return
+        .sort(sort) // Apply sorting
+        .toArray(); // Convert the result to an array
 
-      res.send(result)
-    })
+      res.send(result); // Send the result as a JSON response
+    });
 
-    // Send a ping to confirm a successful connection
+    // Send a ping to confirm a successful connection (optional)
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -99,11 +98,12 @@ async function run() {
 }
 run().catch(console.dir);
 
-
+// Root route to verify the server is running
 app.get('/', (req, res) => {
-  res.send("PRODUCT CATALOG SERVER IN RUNNING")
-})
+  res.send("PRODUCT CATALOG SERVER IS RUNNING");
+});
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log("PRODUCT CATALOG SERVER RUNNING ON", port);
-})
+});
